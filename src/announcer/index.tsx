bfa146bcf9fb407fecf0b713145aa1aa7e62f210
João Dias <joao.dias@feedzai.com>
2020-11-28 01:58:44 +0000

    Sample components

---
+++ src/announcer/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Announcer = () => {
+	return <div />;
+};
---
+++ src/rover/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Rover = () => {
+	return <div />;
+};
